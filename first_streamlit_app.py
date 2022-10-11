dbe6aeaf53aa68a7fd6d4746f29161b7edbffeab
ashu4046 <115546782+ashu4046@users.noreply.github.com>
2022-10-11 16:47:45 +0530

    Update first_streamlit_app.py
    added breaktast menu header and 3 Items

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,2 +1,7 @@
 import Streamlit
 Streamlit.title('I have good understanding od Snowflake concepts')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
